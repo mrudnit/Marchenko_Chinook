68999ef6d98e6bd86002221231b726fb9072ec1d
mrudnit <161849211+mrudnit@users.noreply.github.com>
2025-01-12 20:53:52 +0300

    upload visualizations

---
+++ Chinook_visual.sql
@@ -0,0 +1,39 @@
+-- Average sum of buying track in company
+SELECT c.Company AS Customer, AVG (i.total) AS Average
+FROM fact_invoiceline il
+JOIN dim_customer c ON il.customerId = c.dim_customerId
+JOIN dim_invoice i ON il.invoiceId = i.dim_invoiceId
+GROUP BY c.Company 
+ORDER BY Average DESC
+LIMIT 10;
+
+-- Favored media types
+SELECT t.MediaType AS MediaType, COUNT(t.dim_trackId) AS TotalTrack
+FROM fact_invoiceline il
+JOIN dim_track t ON il.trackId = t.dim_trackId
+GROUP BY MediaType
+ORDER BY TotalTrack DESC;
+
+-- Sales seasonality
+SELECT d.Year AS Year, d.Month AS Mounth, SUM(il.UnitPrice*il.Quantity) AS TotalSales
+FROM fact_invoiceline il
+JOIN dim_date d ON il.dateid = d.dim_dateId
+GROUP BY d.Year, d.Month
+ORDER BY TotalSales DESC
+LIMIT 100;
+
+-- Geographically the best selling point
+SELECT a.Country AS Country, SUM(il.Quantity) AS TrackSold
+FROM fact_invoiceline il
+JOIN dim_address a ON il.addressId = a.dim_addressId
+GROUP BY a.Country
+ORDER BY TrackSold DESC
+LIMIT 5;
+
+-- Most sales Track
+SELECT t.Name AS Track, SUM(il.Quantity) AS TotalSold
+FROM fact_invoiceline il
+JOIN dim_track t ON il.trackId = t.dim_trackId
+GROUP BY t.Name 
+ORDER BY TotalSold DESC
+LIMIT 10;
